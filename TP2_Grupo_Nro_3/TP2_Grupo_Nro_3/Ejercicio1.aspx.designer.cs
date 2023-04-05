11313d2e90d625bfafee038deefeceff0f0ebcb5
Mateo Coronel <mateocoronel240@gmail.com>
2023-04-05 13:39:35 -0300

    Aporte 5 del ejercicio 1 realizado

--- TP2_Grupo_Nro_3/TP2_Grupo_Nro_3/Ejercicio1.aspx.designer.cs
+++ TP2_Grupo_Nro_3/TP2_Grupo_Nro_3/Ejercicio1.aspx.designer.cs
@@ -58,5 +58,23 @@ namespace TP2_Grupo_Nro_3
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox txtCantidad2;
+
+        /// <summary>
+        /// Control btnGeneTabla.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnGeneTabla;
+
+        /// <summary>
+        /// Control lblTabla.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblTabla;
     }
 }
